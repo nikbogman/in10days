8735b5b1a9914c96d100c7f1951c739f01d1c2c5
Nicola Manoilov <i500555@fhict.nl>
2023-09-28 11:38:05 +0200

    sitemap update

--- events-api/src/auth.guard.ts
+++ events-api/src/auth.guard.ts
@@ -1,1 +1,12 @@
-export class AuthGuard {}
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return validateRequest(request);
+  }
+}
